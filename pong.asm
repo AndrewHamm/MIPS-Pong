81aea2b6c1023f6e339154df69a996a856ff4bc3
Billy Barbaro <billy@thebarbaros.com>
2015-04-23 01:47:07 -0400

    Drew a P2

--- pong.asm
+++ pong.asm
@@ -450,6 +450,22 @@ NewGame:
 		lw $a2, ballColor
 		jal DrawPoint
 		
+		WinOne:	li $a0, 34 #the x coordinate
+		li $a1, 12 #the y starting coordinate
+		lw $a2, ballColor #the color
+		li $a3, 15 #the y ending coordinate
+		jal DrawVerticalLine
+		
+		li $a0, 33
+		li $a1, 13
+		lw $a2, ballColor
+		jal DrawPoint
+		
+		li $a0, 33 #the x starting coordinate
+		li $a1, 16 #the y coordinate
+		lw $a2, ballColor #the color
+		li $a3, 35 #the x ending coordinate
+		jal DrawHorizontalLine
 		
 	
 SelectMode:
@@ -478,22 +494,44 @@ SelectMode:
 		li $a3, 29 #the ending x coordinate
 		jal DrawHorizontalLine
 		
-	WinOne:	li $a0, 34 #the x coordinate
-		li $a1, 12 #the y starting coordinate
+	WinTwo:	li $a0, 33 #the x starting coordinate
+		li $a1, 16 #the y coordinate
 		lw $a2, ballColor #the color
-		li $a3, 15 #the y ending coordinate
-		jal DrawVerticalLine
-		
+		li $a3, 36 #the x ending coordinate
+		jal DrawHorizontalLine
+	
+		li $a0, 34 #the x starting coordinate
+		li $a1, 12 #the y coordinate
+		lw $a2, ballColor #the color
+		li $a3, 35 #the x ending coordinate
+		jal DrawHorizontalLine
+	
+		li $a0, 35
+		li $a1, 16
+		lw $a2, ballColor
+		jal DrawPoint
+	
+		li $a0, 34
+		li $a1, 15
+		lw $a2, ballColor
+		jal DrawPoint
+	
+		li $a0, 35
+		li $a1, 14
+		lw $a2, ballColor
+		jal DrawPoint
+	
+		li $a0, 36
+		li $a1, 13
+		lw $a2, ballColor
+		jal DrawPoint
+	
 		li $a0, 33
 		li $a1, 13
 		lw $a2, ballColor
 		jal DrawPoint
-		
-		li $a0, 33 #the x starting coordinate
-		li $a1, 16 #the y coordinate
-		lw $a2, ballColor #the color
-		li $a3, 35 #the x ending coordinate
-		jal DrawHorizontalLine
+
+
 
 		lw $t1, 0xFFFF0004		# check to see which key has been pressed
 		beq $t1, 0x00000031, SetOnePlayerMode # 1 pressed
