dd0ecd88c2ebd1c6c88359fa1e37c6c21b7424ed
Billy Barbaro <billy@thebarbaros.com>
2015-04-22 21:31:53 -0400

    Added the o in or

--- display.asm
+++ display.asm
@@ -217,6 +217,29 @@ One:
 	lw $a2, ballColor #the color
 	li $a3, 36 #the x ending coordinate
 	jal DrawHorizontalLine
+	
+OinOr:
+	li $a0, 39 #the x coordinate
+	li $a1, 25 #the y starting coordinate
+	lw $a2, ballColor #the color
+	li $a3, 29 #the y ending coordinate
+	jal DrawVerticalLine
+
+	li $a0, 41 #the x coordinate
+	li $a1, 25 #the y starting coordinate
+	lw $a2, ballColor #the color
+	li $a3, 29 #the y ending coordinate
+	jal DrawVerticalLine	
+	
+	li $a0, 40
+	li $a1, 25
+	lw $a2, ballColor
+	jal DrawPoint
+	
+	li $a0, 40
+	li $a1, 29
+	lw $a2, ballColor
+	jal DrawPoint
 
 NewGame:
 		
