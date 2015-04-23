007da7765f55365228f5418891991b91926f6e04
Billy Barbaro <billy@thebarbaros.com>
2015-04-22 20:50:40 -0400

    Made the N.

--- display.asm
+++ display.asm
@@ -35,6 +35,36 @@ lw $a2, ballColor
 li $a3, 16 
 jal DrawVerticalLine
 
+#33 over 13 down.  6 tall
+li $a0, 33 #the x coordinate
+li $a1, 13 #the y starting coordinate
+lw $a2, ballColor #the color
+li $a3, 18 #the y ending coordinate
+jal DrawVerticalLine
+
+li $a0, 34 #the x coordinate
+li $a1, 13 #the y starting coordinate
+lw $a2, ballColor #the color
+li $a3, 14 #the y ending coordinate
+jal DrawVerticalLine
+
+li $a0, 35 #the x coordinate
+li $a1, 15 #the y starting coordinate
+lw $a2, ballColor #the color
+li $a3, 16 #the y ending coordinate
+jal DrawVerticalLine
+
+li $a0, 36
+li $a1, 17
+lw $a2, ballColor
+jal DrawPoint
+
+li $a0, 37 #the x coordinate
+li $a1, 13 #the y starting coordinate
+lw $a2, ballColor #the color
+li $a3, 18 #the y ending coordinate
+jal DrawVerticalLine
+
 NewGame:
 		
 		# 1 is 0x00000031
